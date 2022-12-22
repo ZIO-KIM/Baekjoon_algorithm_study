dbaed9954f3670da0716c00b92586e9ba7a885c7
sunghyouk <sakebite@snu.ac.kr>
2022-12-22 11:53:37 +0900

    Solution) 1008

---
+++ SungHyouk/CPP_codeup_basic/1008.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "\u250c\u252c\u2510" << std::endl;
+    std::cout << "\u251c\u253c\u2524" << std::endl;
+    std::cout << "\u2514\u2534\u2518";
+    return 0;
+}
