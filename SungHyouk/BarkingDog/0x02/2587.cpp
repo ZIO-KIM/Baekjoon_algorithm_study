66a3900357991fc83fc00d3269c0ce6beece7b25
sunghyouk <sakebite@snu.ac.kr>
2023-01-09 15:13:46 +0900

    [Bronze 2] Solution) 2587, 대표값2

---
+++ SungHyouk/BarkingDog/0x02/2587.cpp
@@ -0,0 +1,42 @@
+/*
+==============================================================================================
+@ Title: 대표값2
+@ URL: https://www.acmicpc.net/problem/2587
+@ Author: Sunghyouk Choi
+@ Created Date: 2023. 1. 9. 오후 3:13:02
+@ Level: Bronze 2
+@ Tags: arithmetic implementation math sorting
+@ Description:
+어떤 수들이 있을 때, 그 수들을 대표하는 값으로 가장 흔하게 쓰이는 것은 평균이다. 평균은 주어진 모든 수의 합을 수의 개수로 나눈
+것이다. 예를 들어 10, 40, 30, 60, 30의 평균은 (10 + 40 + 30 + 60 + 30) / 5 = 170 / 5 =
+34가 된다. 평균 이외의 또 다른 대표값으로 중앙값이라는 것이 있다. 중앙값은 주어진 수를 크기 순서대로 늘어 놓았을 때 가장 중앙에
+놓인 값이다. 예를 들어 10, 40, 30, 60, 30의 경우, 크기 순서대로 늘어 놓으면 10 30 30 40 60 이 되고
+따라서 중앙값은 30이 된다. 다섯 개의 자연수가 주어질 때 이들의 평균과 중앙값을 구하는 프로그램을 작성하시오.
+@ Input: 첫째 줄부터 다섯 번째 줄까지 한 줄에 하나씩 자연수가 주어진다. 주어지는 자연수는 100 보다 작은 10의 배수이다.
+@ Output: 첫째 줄에는 평균을 출력하고, 둘째 줄에는 중앙값을 출력한다. 평균과 중앙값은 모두 자연수이다.
+==============================================================================================
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    vector<int> numbers;
+    int number = 0;
+    for (int i=0; i<5; i++) {
+        cin >> number;
+        numbers.push_back(number);
+    }
+    int sum = 0;
+    for (int x: numbers)
+        sum += x;
+    sort(numbers.begin(), numbers.end());
+    cout << sum/numbers.size() << '\n' << numbers[numbers.size()/2];
+
+    return 0;
+}
