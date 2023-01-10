ee9f2d65e851258c3e11ce38053191410c4d94e9
sunghyouk <sakebite@snu.ac.kr>
2023-01-10 13:32:46 +0900

    [Bronze 3] Solution) 2443, 별 찍기 - 6

---
+++ SungHyouk/BarkingDog/0x02/2443.cpp
@@ -0,0 +1,39 @@
+/*
+==============================================================================================
+@ Title: 별 찍기 - 6
+@ URL: https://www.acmicpc.net/problem/2443
+@ Author: Sunghyouk Choi
+@ Created Date: 2023. 1. 10. 오후 1:32:11
+@ Level: Bronze 3
+@ Tags: implementation
+@ Description:
+첫째 줄에는 별 2×N-1개, 둘째 줄에는 별 2×N-3개, ..., N번째 줄에는 별 1개를 찍는 문제 별은 가운데를 기준으로
+대칭이어야 한다.
+@ Input: 첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+@ Output: 첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+==============================================================================================
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<i; j++) {
+            cout << " ";
+        }
+        for (int k=0; k<2*(n-i)-1; k++) {
+            cout << "*";
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
