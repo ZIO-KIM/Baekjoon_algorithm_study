80baa0b89308eff89026d02df620456b8ba5d1f4
sunghyouk <sakebite@snu.ac.kr>
2023-01-11 12:40:56 +0900

    [Bronze 3] Solution) 2446, 별 찍기 - 9

---
+++ SungHyouk/BarkingDog/0x02/2446.cpp
@@ -0,0 +1,45 @@
+/*
+==============================================================================================
+@ Title: 별 찍기 - 9
+@ URL: https://www.acmicpc.net/problem/2446
+@ Author: Sunghyouk Choi
+@ Created Date: 2023. 1. 11. 오후 12:40:33
+@ Level: Bronze 3
+@ Tags: implementation
+@ Description:
+예제를 보고 규칙을 유추한 뒤에 별을 찍어 보세요.
+@ Input: 첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+@ Output: 첫째 줄부터 2×N-1번째 줄까지 차례대로 별을 출력한다.
+==============================================================================================
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<i; j++) {
+            cout << ' ';
+        }
+        for (int k=0; k<2*(n-i)-1; k++) {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+    for (int i=1; i<n; i++) {
+        for (int j=1; j<n-i; j++) {
+            cout << ' ';
+        }
+        for (int k=0; k<2*i+1; k++) {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
