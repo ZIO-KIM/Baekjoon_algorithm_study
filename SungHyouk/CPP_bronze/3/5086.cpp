3cc5f5bde5843cdd020dea84b58f6f7bce029464
sunghyouk <sakebite@snu.ac.kr>
2023-01-31 11:19:05 +0900

    [Bronze 3] Solve 5086, 배수와 약수 https://www.acmicpc.net/problem/5086

---
+++ SungHyouk/CPP_bronze/3/5086.cpp
@@ -0,0 +1,38 @@
+/*
+==============================================================================================
+@ Title: 배수와 약수
+@ URL: https://www.acmicpc.net/problem/5086
+@ Author: shc
+@ Created Date: 2023. 1. 31. 오전 11:01:11
+@ Level: Bronze 3
+@ Tags: arithmetic math
+@ Description:
+4 × 3 = 12이다. 이 식을 통해 다음과 같은 사실을 알 수 있다. 3은 12의 약수이고, 12는 3의 배수이다. 4도 12의
+약수이고, 12는 4의 배수이다. 두 수가 주어졌을 때, 다음 3가지 중 어떤 관계인지 구하는 프로그램을 작성하시오. 첫 번째 숫자가
+두 번째 숫자의 약수이다. 첫 번째 숫자가 두 번째 숫자의 배수이다. 첫 번째 숫자가 두 번째 숫자의 약수와 배수 모두 아니다.
+@ Input: 입력은 여러 테스트 케이스로 이루어져 있다. 각 테스트 케이스는 10,000이 넘지않는 두 자연수로 이루어져 있다. 마지막 줄에는 0이
+2개 주어진다. 두 수가 같은 경우는 없다.
+@ Output: 각 테스트 케이스마다 첫 번째 숫자가 두 번째 숫자의 약수라면 factor를, 배수라면 multiple을, 둘 다 아니라면
+neither를 출력한다.
+==============================================================================================
+*/
+#include <iostream>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int front = 10001;
+    int end = 10001;
+
+    while (true) {
+        cin >> front >> end;
+        if (front != 0 && end != 0) {
+            if (end % front == 0) cout << "factor\n";
+            else if (front % end == 0) cout << "multiple\n";
+            else cout << "neither\n";
+        }
+        else break;
+    }
+
+    return 0;
+}
