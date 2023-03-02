dc8f333d867f72e6133b68c47a82413de84faabd
sunghyouk <sakebite@snu.ac.kr>
2023-03-02 17:07:33 +0900

    Solution) using Fortran

---
+++ SungHyouk/etc_lang/2321.f
@@ -0,0 +1,8 @@
+program p
+    print '(a)', ',~'
+    print '(a)', '( 9> //)'
+    print '(a)', ' )(_///'
+    print '(a)', '( \_>/'
+    print '(a)', ' \__/'
+    print '(a)', '  ^^'
+  end program p
