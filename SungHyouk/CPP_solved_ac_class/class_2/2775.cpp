cd2ee559e9ea6871d8b4adebac2ef02e28b715ce
sunghyouk <sakebite@snu.ac.kr>
2023-03-14 14:21:04 +0900

    [Bronze 1] Solution) 2775, 부녀회장이 될테야

---
+++ SungHyouk/CPP_solved_ac_class/class_2/2775.cpp
@@ -0,0 +1,41 @@
+/*
+==============================================================================================
+@ Title: 부녀회장이 될테야
+@ URL: https://www.acmicpc.net/problem/2775
+@ Author: Sunghyouk Choi
+@ Created Date: 2023. 3. 14. 오후 2:17:37
+@ Level: Bronze 1
+@ Tags: dp implementation math
+@ Description:
+평소 반상회에 참석하는 것을 좋아하는 주희는 이번 기회에 부녀회장이 되고 싶어 각 층의 사람들을 불러 모아 반상회를 주최하려고 한다.
+이 아파트에 거주를 하려면 조건이 있는데, “a층의 b호에 살려면 자신의 아래(a-1)층의 1호부터 b호까지 사람들의 수의 합만큼
+사람들을 데려와 살아야 한다” 는 계약 조항을 꼭 지키고 들어와야 한다. 아파트에 비어있는 집은 없고 모든 거주민들이 이 계약 조건을
+지키고 왔다고 가정했을 때, 주어지는 양의 정수 k와 n에 대해 k층에 n호에는 몇 명이 살고 있는지 출력하라. 단, 아파트에는
+0층부터 있고 각층에는 1호부터 있으며, 0층의 i호에는 i명이 산다.
+@ Input: 첫 번째 줄에 Test case의 수 T가 주어진다. 그리고 각각의 케이스마다 입력으로 첫 번째 줄에 정수 k, 두 번째 줄에 정수
+n이 주어진다
+@ Output: 각각의 Test case에 대해서 해당 집에 거주민 수를 출력하라.
+==============================================================================================
+*/
+#include <iostream>
+using namespace std;
+
+int my_family(int a, int b) {
+    if (b==1) return 1;
+    if (a==0) return b;
+    return my_family(a-1, b) + my_family(a, b-1);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T, k, n;
+    cin >> T;
+
+    for (int i=0; i<T; ++i) {
+        cin >> k >> n;
+        cout << my_family(k, n) << '\n';
+    }
+    return 0;
+}
